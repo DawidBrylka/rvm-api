1ff72fd7690cd097ba7cc262943465f787396a84
dawid.brylka@kaucja.pl <dawid.brylka@kaucja.pl>
2025-03-24 14:06:57 +0100

    Added Serial Number to Machine

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI3NjQ5MA/spectral.js?branch_slug=main&token=d748f247-1044-4174-84b4-01c87b6737f9";
+export default { extends: ruleset };
